dde9ccf7d5b153892298a8d7969d44d0b661d17b
Martin Delille <martin@delille.org>
2024-12-07 12:52:48 +0100

    Fix linting errors

--- friction/friction.ts
+++ friction/friction.ts
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { World, Material, Plane, Body, Box, Vec3 } from "cannon-es";
 
 const scene = new THREE.Scene();
